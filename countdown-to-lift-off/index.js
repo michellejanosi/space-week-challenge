5135b75b7f2fe17a62d579b28a250ab76bf75f7c
michellejanosi <michelle.janosi@gmail.com>
2022-06-22 05:59:37 -0400

    Add countdown timer function

--- countdown-to-lift-off/index.js
+++ countdown-to-lift-off/index.js
@@ -1,8 +1,25 @@
 const countdownDisplay = document.getElementById("countdown-display");
 const rocket = document.getElementById("rocket");
-let timeRemaining = 60;
+let timeRemaining = 10;
+let timerInterval = null;
+let timePassed = 0;
+let timeLeft = timeRemaining;
 
+function startTimer() {
+  timerInterval = setInterval(() => {
 
+    timePassed = timePassed += 1;
+    timeLeft = timeRemaining - timePassed;
+
+    countdownDisplay.innerHTML = timeLeft;
+
+    if (timeLeft < 0) {
+      countdownDisplay.innerHTML = 'Liftoff!';
+    }
+  }, 1000);
+}
+
+startTimer();
 
 // Task:
 // 1. Write the JavaScript to count down from 60 to 0 and display the remaining time
